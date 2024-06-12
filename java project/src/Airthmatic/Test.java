350e64725f91c9dda72ddac3e6c61e5160fed4d1
shrutirai <shrutirai200826@gmail.com>
2024-06-12 22:04:12 +0530

    First code commit

---
+++ java project/src/Airthmatic/Test.java
@@ -0,0 +1,15 @@
+package Airthmatic;
+
+public class Test {
+	int a;
+	public void display()
+	{
+		System.out.println("First Programe ");
+	}
+public static void main(String[] args) {
+	Test abc = new Test();
+	abc.display();
+	abc.a= 123;
+	System.out.println("value of a is "+abc.a);
+}
+}
